b59d77352bf9107b82925d1c7caf36de657151b1
Aswin Matthews Ashok <abcaswin94@gmail.com>
2019-09-29 14:41:09 -0400

    Project bonus started

---
+++ project_2 V1 - Bonus/lib/gossip/boss.ex
@@ -0,0 +1,16 @@
+defmodule Gossip.Boss do
+	def manage(actors,key\\0) do
+		if not Enum.any?(0..tuple_size(actors)-1, fn i -> elem(actors,i)["state"] == "alive" end) do
+			unaware_nodes = Gossip.Topologies.get_unaware(actors)
+			if not Enum.empty?(unaware_nodes) do
+				chosen = Enum.random(unaware_nodes)
+				GenServer.cast(elem(actors,chosen)["pid"],{:seed})
+			end
+		end
+		actors = Gossip.Topologies.update_actor_states(actors,key)
+		Process.sleep(10)
+		if Enum.any?(0..tuple_size(actors)-1, fn i -> elem(actors,i)["state"]!= "dead" end) do
+			manage(actors,key+1)
+		end
+	end
+end
---
+++ project_2 V1 - Bonus/lib/proj2.ex
@@ -0,0 +1,50 @@
+defmodule Gossip.Proj2 do
+	def check_args(args) do
+		if length(args) == 3 do
+			[numNodes, topology, algorithm] = args
+			try do
+				numNodes = String.to_integer(numNodes)
+				if is_integer(numNodes) and numNodes > 0 do
+					if Enum.member?(["full", "line", "rand2D", "3Dtorus", "honeycomb" , "randhoneycomb"], topology) do
+						if Enum.member?(["gossip", "push-sum"], algorithm) do
+							true
+						else
+							IO.puts("Invalid algorithm")
+							false
+						end
+					else
+						IO.puts("Invalid topology")
+						false
+					end
+				else
+					IO.puts("Invalid number of nodes")
+					false
+				end
+			rescue
+				ArgumentError -> 
+					IO.puts("Invalid number of nodes")
+					false
+			end
+		else
+			IO.puts("Invalid number of arguments")
+			false
+		end
+	end
+	def main(args\\[]) do
+		script_mode = true
+		if check_args(args) do
+			[numNodes, topology, algorithm] = args
+			actors = Gossip.Topologies.build_topology(topology,String.to_integer(numNodes),algorithm)
+			start = Time.utc_now()
+			Gossip.Boss.manage(actors)
+			stop = Time.utc_now()
+			Gossip.Topologies.terminate_actors(actors)
+			time_taken = Time.diff(stop,start,:millisecond)
+			if script_mode do
+				IO.puts("#{time_taken}")
+			else
+				IO.puts("Time taken to achieve convergence is #{time_taken} milliseconds")
+			end
+		end
+	end
+end
